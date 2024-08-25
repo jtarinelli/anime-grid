70caffb9f9722df4615a5812e34428e80c82650f
jtarinelli <tarin22j@mtholyoke.edu>
2024-08-24 22:10:00 -0400

    update result emoji size, copy text

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -9,8 +9,8 @@ interface ResultsProps {
 const Results: FC<ResultsProps> = ({ numberOfClues, guesses }) => {
     let resultGrid = "";
 
-    for (let row = 1; row < (numberOfClues / 2) + 1; row ++) {
-        for (let col = 1; col < (numberOfClues / 2) + 1; col ++) {
+    for (let row = 1; row < (numberOfClues / 2) + 1; row++) {
+        for (let col = 1; col < (numberOfClues / 2) + 1; col++) {
             const guess = guesses.find(guess => guess.cellCoordinates.row === row && guess.cellCoordinates.col === col);
             if (guess?.isCorrect) {
                 resultGrid = resultGrid.concat('🟩')
@@ -25,15 +25,18 @@ const Results: FC<ResultsProps> = ({ numberOfClues, guesses }) => {
     const numberCorrectGuesses = guesses.filter(guess => guess.isCorrect).length;
 
     const onCopy = () => {
-        navigator.clipboard.writeText(resultGrid);
+        const copyText = "Anime Grid\n".concat(resultGrid);
+        navigator.clipboard.writeText(copyText);
         // apparently this won't always work so should prob have fallback/error handling
     }
 
-    return <div style={{whiteSpace: "pre-wrap"}}>
+    return <div>
         {`${numberCorrectGuesses}/${numberGuesses} correct`}
-        <br/>
-        {resultGrid}
-        <br/>
+        <br />
+        <div style={{ whiteSpace: "pre-wrap" }} className="text-2xl">
+            {resultGrid}
+        </div>
+        <br />
         <button onClick={onCopy} className="border-2 p-2">Copy</button>
     </div>
 }
