36125a8a5a50b88a44773613dcf95010a5f9d9c6
jtarinelli <tarin22j@mtholyoke.edu>
2024-08-24 23:11:46 -0400

    add styling to pop up

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -7,10 +7,14 @@ interface PopupProps {
 
 const Popup: FC<PopupProps> = ({ children, onClose }) => {
 
-    return <div>
-        <button onClick={onClose}>X</button>
-        <br />
-        {children}
+    return <div className="absolute h-full w-full flex place-content-center items-center bg-black/25">
+        <div className="bg-white text-center w-fit h-fit border-2 p-4">
+            <div className="text-right">
+                <button onClick={onClose} className="border-2 p-1">X</button>
+            </div>
+            <br />
+            {children}
+        </div>
     </div>
 }
 
