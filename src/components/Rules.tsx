53e1606a64d36f46275c6ba5cf62271c915ea490
jtarinelli <tarin22j@mtholyoke.edu>
2024-09-05 21:40:24 -0400

    add movie rule

--- src/components/Rules.tsx
+++ src/components/Rules.tsx
@@ -12,6 +12,7 @@ export const Rules: FC<RulesProps> = ({ onClose }) => {
             <li className='mb-2'>You get nine guesses total, including incorrect ones.</li>
             <li className='mb-2'>Make sure you've selected your guess from the dropdown before guessing or it shan't work.</li>
             <li className='mb-2'>Correct guesses can't be reused.</li>
+            <li className='mb-2'>Movies are considered incorrect guesses for clues about the number of episodes.</li>
             <li className='mb-2'>Words are only counted as separate if separated by a space, any other character like a dash doesn't count. For example "Mahou Shoujo Madoka★Magica" is considered three words.</li>
             <li className='mb-2'>Clues relating to anime titles are checked against the romaji version of the Japanese title, plus the official English version if there is one. For example "Kuragehime" counts as a one word title even though the English title "Princess Jellyfish" is two words. If in doubt, both versions are displayed in the dropdown.</li>
         </ul>
