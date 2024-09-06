6779ea8e10b9f2fe41cb21a3ab59c6f97d985670
jtarinelli <tarin22j@mtholyoke.edu>
2024-09-06 19:35:46 -0400

    make rules seem more clickable

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -11,7 +11,7 @@ const Menu: FC = () => {
         <div className='md:h-full md:place-self-start p-6 md:p-8 bg-slate-100'>
             <ul className='flex md:flex-col justify-evenly align-bottom'>
                 <li><h1 className='text-lg md:mb-10'>Anime Grid</h1></li>
-                <li className='md:mb-4'><a onClick={() => setShowRules(true)}>Rules</a></li>
+                <li className='md:mb-4'><a onClick={() => setShowRules(true)} className="cursor-pointer">Rules</a></li>
                 <li className='md:mb-4 md:hidden'><a onClick={() => setShowGameModes(!showGameModes)}>Mode: Normal v</a>
                 <ul className={`absolute${!showGameModes ? ' hidden' : ''} bg-slate-100 p-4 border-2 mt-2`}>
                     <li className='mb-2'>Normal</li>
