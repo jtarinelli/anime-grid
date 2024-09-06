e794c2fa9e4a6869a24ffbc01d5cebf81f6ec8df
jtarinelli <tarin22j@mtholyoke.edu>
2024-09-05 21:35:39 -0400

    add game mode dropdown for mobile

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,17 +3,27 @@ import { Rules } from "./Rules";
 
 const Menu: FC = () => {
     const [showRules, setShowRules] = useState<boolean>(false);
+    const [showGameModes, setShowGameModes] = useState<boolean>(false);
 
-    // make the game mode a label on big screen, clickable dropdown on mobile
+    // make the game mode say current mode (mode: normal) 
+    // (is it clear that it's changable tho? need lil dropdown symbol?)
     return <>
         <div className='md:h-full md:place-self-start p-6 md:p-8 bg-slate-100'>
-            <h1 className='text-lg md:mb-10'>Anime Grid</h1>
-            <ul className='flex md:flex-col justify-evenly'>
+            <ul className='flex md:flex-col justify-evenly align-bottom'>
+                <li><h1 className='text-lg md:mb-10'>Anime Grid</h1></li>
                 <li className='md:mb-4'><a onClick={() => setShowRules(true)}>Rules</a></li>
-                {/* <li className='md:mb-4'><p>Game modes</p></li>
-                <li>Normal</li>
-                <li>Normal</li>
-                <li>Normal</li> */}
+                <li className='md:mb-4 md:hidden'><a onClick={() => setShowGameModes(!showGameModes)}>Game modes</a>
+                <ul className={`absolute${!showGameModes ? ' hidden' : ''} bg-slate-100 p-4 border-2 mt-2`}>
+                    <li className='mb-2'>Normal</li>
+                    {/* <li className='mb-2'>Voice actors</li>
+                    <li className=''>Random</li> */}
+                </ul>
+                </li>
+                <ul className={`hidden md:block`}>
+                    <li className='mb-4'>Normal</li>
+                    {/* <li className='mb-4'>Voice actors</li>
+                    <li className='mb-4'>Random</li> */}
+                </ul>
             </ul>
         </div>
         {showRules && <Rules onClose={() => setShowRules(false)} />}
