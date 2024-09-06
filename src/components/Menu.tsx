9001da5d7428a8e515517ba4c8a395dbeb8aef01
jtarinelli <tarin22j@mtholyoke.edu>
2024-09-05 22:26:41 -0400

    update game mode label

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -12,7 +12,7 @@ const Menu: FC = () => {
             <ul className='flex md:flex-col justify-evenly align-bottom'>
                 <li><h1 className='text-lg md:mb-10'>Anime Grid</h1></li>
                 <li className='md:mb-4'><a onClick={() => setShowRules(true)}>Rules</a></li>
-                <li className='md:mb-4 md:hidden'><a onClick={() => setShowGameModes(!showGameModes)}>Game modes</a>
+                <li className='md:mb-4 md:hidden'><a onClick={() => setShowGameModes(!showGameModes)}>Mode: Normal v</a>
                 <ul className={`absolute${!showGameModes ? ' hidden' : ''} bg-slate-100 p-4 border-2 mt-2`}>
                     <li className='mb-2'>Normal</li>
                     {/* <li className='mb-2'>Voice actors</li>
@@ -20,6 +20,7 @@ const Menu: FC = () => {
                 </ul>
                 </li>
                 <ul className={`hidden md:block`}>
+                    <li className='mb-4'>Mode:</li>
                     <li className='mb-4'>Normal</li>
                     {/* <li className='mb-4'>Voice actors</li>
                     <li className='mb-4'>Random</li> */}
