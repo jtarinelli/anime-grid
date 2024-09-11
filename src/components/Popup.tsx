794989929d514b9e121f2b6743c41a3d7d80284c
jtarinelli <tarin22j@mtholyoke.edu>
2024-09-10 21:48:50 -0400

    fix overlay for popup

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -9,7 +9,7 @@ interface PopupProps {
 const Popup: FC<PopupProps> = ({ children, onClose, title }) => {
 
     // make it so just the text body scrolls and not the title/x button
-    return <div className="fixed top-0 left-0 h-full w-full flex place-content-center items-center bg-black/25">
+    return <div className="fixed top-0 left-0 h-full w-full flex place-content-center items-center bg-neutral-800/50">
         <div className="bg-background text-center min-w-80 max-w-lg h-auto max-h-[90%] overflow-y-auto border-2 border-neutral-300 *:m-4">
             <div className="sticky top-0 bg-background p-4 pb-0">
                 <div className="text-right h-2">
