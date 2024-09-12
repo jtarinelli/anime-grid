71e0aa9fd7b8b146677f7e9c1f1680720b1a42ed
jtarinelli <tarin22j@mtholyoke.edu>
2024-09-11 21:23:20 -0400

    comment out reset button

--- src/components/Guesses.tsx
+++ src/components/Guesses.tsx
@@ -10,13 +10,14 @@ type GuessesProps = {
 }
 
 export const Guesses: FC<GuessesProps> = ({ guessesLeft, isGameOver, onReset, onGiveUp, onShowResults }) => {
+    const resetKaomoji = "(*/ω＼*)";
     return (
         <>
             <div className="p-10 text-center">
                 <h2 className='text-lg'>{isGameOver ? 0 : guessesLeft} guesses left!!!</h2>
                 <br />
-                <Button label="Reset (*/ω＼*)" onClick={onReset} />
-                <br /><br />
+                {/* <Button label=`Reset {resetKaomoji}` onClick={onReset} />
+                <br /><br /> */}
                 {!isGameOver &&
                 <Button label="Give up (；′⌒`)" onClick={onGiveUp} />}
                 {isGameOver &&
