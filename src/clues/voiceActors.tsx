01f36ab336cad0418ed04d5fcdf7070345a1ca4e
jtarinelli <tarin22j@mtholyoke.edu>
2024-09-14 09:31:16 -0400

    add top 4 pages of most popular VAs

---
+++ src/clues/voiceActors.tsx
@@ -0,0 +1,602 @@
+// maybe remove some with less main/well-known roles (less than 3?)
+// also idk if one piece / detective conan should count for years active
+
+export const voiceActors = [
+    {
+        "value": "Hiroshi Kamiya",
+        "yearsActive": {
+            "min": 1994
+        }
+    },
+    {
+        "value": "Kenjirou Tsuda",
+        "yearsActive": {
+            "min": 1995
+        }
+    },
+    {
+        "value": "Saori Hayami",
+        "yearsActive": {
+            "min": 2007
+        }
+    },
+    {
+        "value": "Kana Hanazawa",
+        "yearsActive": {
+            "min": 2003
+        }
+    },
+    {
+        "value": "Mamoru Miyano",
+        "yearsActive": {
+            "min": 2001
+        }
+    },
+    {
+        "value": "Yuuki Kaji",
+        "yearsActive": {
+            "min": 2004
+        }
+    },
+    {
+        "value": "Rie Takahashi",
+        "yearsActive": {
+            "min": 2013
+        }
+    },
+    {
+        "value": "Aoi Yuuki",
+        "yearsActive": {
+            "min": 2003
+        }
+    },
+    {
+        "value": "Nobuhiko Okamoto",
+        "yearsActive": {
+            "min": 2006
+        }
+    },
+    {
+        "value": "Takehito Koyasu",
+        "yearsActive": {
+            "min": 1988
+        }
+    },
+    {
+        "value": "Yuuichi Nakamura",
+        "yearsActive": {
+            "min": 2001
+        }
+    },
+    {
+        "value": "Yoshitsugu Matsuoka",
+        "yearsActive": {
+            "min": 2009
+        }
+    },
+    {
+        "value": "Daisuke Ono",
+        "yearsActive": {
+            "min": 2001
+        }
+    },
+    {
+        "value": "Miyuki Sawashiro",
+        "yearsActive": {
+            "min": 1999
+        }
+    },
+    {
+        "value": "Tomokazu Sugita",
+        "yearsActive": { min: 2000 }
+    },
+    {
+        "value": "Junichi Suwabe",
+        "yearsActive": {
+            "min": 1995
+        }
+    },
+    {
+        "value": "Inori Minase",
+        "yearsActive": {
+            "min": 2010
+        }
+    },
+    {
+        "value": "Sora Amamiya",
+        "yearsActive": {
+            "min": 2012
+        }
+    },
+    {
+        "value": "Natsuki Hanae",
+        "yearsActive": {
+            "min": 2011
+        }
+    },
+    {
+        "value": "Kouki Uchiyama",
+        "yearsActive": {
+            "min": 2003
+        }
+    },
+    {
+        "value": "Takahiro Sakurai",
+        "yearsActive": {
+            "min": 1996
+        }
+    },
+    {
+        "value": "Ai Kayano",
+        "yearsActive": {
+            "min": 2010
+        }
+    },
+    {
+        "value": "Ayane Sakura",
+        "yearsActive": {
+            "min": 2010
+        }
+    },
+    {
+        "value": "Maaya Sakamoto",
+        "yearsActive": {
+            "min": 1992
+        }
+    },
+    {
+        "value": "Rie Kugimiya",
+        "yearsActive": {
+            "min": 1998
+        }
+    },
+    {
+        "value": "Nao Touyama",
+        "yearsActive": {
+            "min": 2010
+        }
+    },
+    {
+        "value": "Jun Fukuyama",
+        "yearsActive": {
+            "min": 1997
+        }
+    },
+    {
+        "value": "Akari Kitou",
+        "yearsActive": {
+            "min": 2014
+        }
+    },
+    {
+        "value": "Maaya Uchida",
+        "yearsActive": {
+            "min": 2010
+        }
+    },
+    {
+        "value": "Yui Ishikawa",
+        "yearsActive": {
+            "min": 2004
+        }
+    },
+    {
+        "value": "Haruka Tomatsu",
+        "yearsActive": {
+            "min": 2007
+        }
+    },
+    {
+        "value": "Aoi Koga",
+        "yearsActive": {
+            "min": 2014
+        }
+    },
+    {
+        "value": "Kaito Ishikawa",
+        "yearsActive": {
+            "min": 2010
+        }
+    },
+    {
+        "value": "Yuuma Uchida",
+        "yearsActive": {
+            "min": 2012
+        }
+    },
+    {
+        "value": "Asami Seto",
+        "yearsActive": {
+            "min": 2010
+        }
+    },
+    {
+        "value": "Ayana Taketatsu",
+        "yearsActive": {
+            "min": 2007
+        }
+    },
+    {
+        "value": "Romi Park",
+        "yearsActive": {
+            "min": 1997
+        }
+    },
+    {
+        "value": "Megumi Ogata",
+        "yearsActive": {
+            "min": 1992
+        }
+    },
+    {
+        "value": "Kazuya Nakai",
+        "yearsActive": {
+            "min": 1995
+        }
+    },
+    {
+        "value": "Akira Ishida",
+        "yearsActive": { min: 1989 }
+    },
+    {
+        "value": "Youko Hikasa",
+        "yearsActive": {
+            "min": 2007
+        }
+    },
+    {
+        "value": "Nobunaga Shimazaki",
+        "yearsActive": {
+            "min": 2009
+        }
+    },
+    {
+        "value": "Hiro Shimono",
+        "yearsActive": {
+            "min": 2001
+        }
+    },
+    {
+        "value": "Mariya Ise",
+        "yearsActive": {
+            "min": 2004
+        }
+    },
+    {
+        "value": "Daisuke Namikawa",
+        "yearsActive": {
+            "min": 1987
+        }
+    },
+    {
+        "value": "Takuya Eguchi",
+        "yearsActive": {
+            "min": 2008
+        }
+    },
+    {
+        "value": "Fairouz Ai",
+        "yearsActive": {
+            "min": 2019
+        }
+    },
+    {
+        "value": "Yoshimasa Hosoya",
+        "yearsActive": {
+            "min": 2004
+        }
+    },
+    {
+        "value": "Atsumi Tanezaki",
+        "yearsActive": {
+            "min": 2008
+        }
+    },
+    {
+        "value": "Kenshou Ono",
+        "yearsActive": {
+            "min": 2001
+        }
+    },
+    {
+        "value": "Konomi Kohara",
+        "yearsActive": {
+            "min": 2012
+        }
+    },
+    {
+        "value": "Yui Horie",
+        "yearsActive": {
+            "min": 1997
+        }
+    },
+    {
+        "value": "Yuusuke Kobayashi",
+        "yearsActive": {
+            "min": 2011
+        }
+    },
+    {
+        "value": "Ayumu Murase",
+        "yearsActive": {
+            "min": 2011
+        }
+    },
+    {
+        "value": "Sumire Uesaka",
+        "yearsActive": { min: 2011 }
+    },
+    {
+        "value": "Reina Ueda",
+        "yearsActive": {
+            "min": 2012
+        }
+    },
+    {
+        "value": "Marina Inoue",
+        "yearsActive": { min: 2004 }
+    },
+    {
+        "value": "Chiwa Saitou",
+        "yearsActive": {
+            "min": 1999
+        }
+    },
+    {
+        "value": "Aki Toyosaki",
+        "yearsActive": {
+            "min": 2006
+        }
+    },
+    {
+        "value": "Tomoyo Kurosawa",
+        "yearsActive": {
+            "min": 2000
+        }
+    },
+    {
+        "value": "Shinichirou Miki",
+        "yearsActive": {
+            "min": 1989
+        }
+    },
+    {
+        "value": "Akio Ootsuka",
+        "yearsActive": {
+            "min": 1988
+        }
+    },
+    {
+        "value": "Yuuichirou Umehara",
+        "yearsActive": { min: 2014 }
+    },
+    {
+        "value": "Miku Itou",
+        "yearsActive": {
+            "min": 2013
+        }
+    },
+    {
+        "value": "Jouji Nakata",
+        "yearsActive": { min: 1988 }
+    },
+    {
+        "value": "Junya Enoki",
+        "yearsActive": {
+            "min": 2010
+        }
+    },
+    {
+        "value": "Mayumi Tanaka",
+        "yearsActive": {
+            "min": 1978
+        }
+    },
+    {
+        "value": "Makoto Furukawa",
+        "yearsActive": {
+            "min": 2011
+        }
+    },
+    {
+        "value": "Nana Mizuki",
+        "yearsActive": { min: 1998 }
+    },
+    {
+        "value": "Yumiri Hanamori",
+        "yearsActive": {
+            "min": 2013
+        }
+    },
+    {
+        "value": "Tomori Kusunoki",
+        "yearsActive": {
+            "min": 2017
+        }
+    },
+    {
+        "value": "Daiki Yamashita",
+        "yearsActive": {
+            "min": 2012
+        }
+    },
+    {
+        "value": "Tatsuhisa Suzuki",
+        "yearsActive": {
+            "min": 2002
+        }
+    },
+    {
+        "value": "Souma Saitou",
+        "yearsActive": {
+            "min": 2010
+        }
+    },
+    {
+        "value": "Aya Hirano",
+        "yearsActive": {
+            "min": 2001
+        }
+    },
+    {
+        "value": "Risa Taneda",
+        "yearsActive": {
+            "min": 2010
+        }
+    },
+    {
+        "value": "Megumi Hayashibara",
+        "yearsActive": {
+            "min": 1986
+        }
+    },
+    {
+        "value": "Tomokazu Seki",
+        "yearsActive": { min: 1986 }
+    },
+    {
+        "value": "Yukari Tamura",
+        "yearsActive": {
+            "min": 1997
+        }
+    },
+    {
+        "value": "Kaede Hondo",
+        "yearsActive": {
+            "min": 2015
+        }
+    },
+    {
+        "value": "Toshiyuki Morikawa",
+        "yearsActive": {
+            "min": 1987
+        }
+    },
+    {
+        "value": "Chika Anzai",
+        "yearsActive": {
+            "min": 2009
+        }
+    },
+    {
+        "value": "Eri Kitamura",
+        "yearsActive": { min: 2003 }
+    },
+    {
+        "value": "Yuu Kobayashi",
+        "yearsActive": {
+            "min": 2003
+        }
+    },
+    {
+        "value": "Ami Koshimizu",
+        "yearsActive": {
+            "min": 2003
+        }
+    },
+    {
+        "value": "Mao Ichimichi",
+        "yearsActive": {
+            "min": 2010
+        }
+    },
+    {
+        "value": "Mamiko Noto",
+        "yearsActive": {
+            "min": 2000
+        }
+    },
+    {
+        "value": "Megumi Han",
+        "yearsActive": { min: 1996 }
+    },
+    {
+        "value": "Ryouhei Kimura",
+        "yearsActive": {
+            "min": 1987
+        }
+    },
+    {
+        "value": "Tomoaki Maeno",
+        "yearsActive": {
+            "min": 2003
+        }
+    },
+    {
+        "value": "Katsuyuki Konishi",
+        "yearsActive": {
+            "min": 1996
+        }
+    },
+    {
+        "value": "Rina Hidaka",
+        "yearsActive": {
+            "min": 2008
+        }
+    },
+    {
+        "value": "Miyu Tomita",
+        "yearsActive": {
+            "min": 2015
+        }
+    },
+    {
+        "value": "Mikako Komatsu",
+        "yearsActive": {
+            "min": 2010
+        }
+    },
+    {
+        "value": "Satoshi Hino",
+        "yearsActive": {
+            "min": 1997
+        }
+    },
+    {
+        "value": "Noriaki Sugiyama",
+        "yearsActive": {
+            "min": 1999
+        }
+    },
+    {
+        "value": "Keiji Fujiwara",
+        "yearsActive": {
+            "min": 1990,
+            "max": 2020
+        }
+    },
+    {
+        "value": "Shizuka Itou",
+        "yearsActive": { min: 2001 }
+    },
+    {
+        "value": "Chiaki Kobayashi",
+        "yearsActive": { min: 2018 }
+    },
+    {
+        "value": "Kana Ichinose",
+        "yearsActive": {
+            "min": 2016
+        }
+    },
+    {
+        "value": "Misaki Kuno",
+        "yearsActive": {
+            "min": 1999
+        }
+    },
+    {
+        "value": "Hikaru Midorikawa",
+        "yearsActive": {
+            "min": 1988
+        }
+    },
+    {
+        "value": "Sayaka Oohara",
+        "yearsActive": {
+            "min": 1998
+        }
+    },
+    { value: "Yuuki Ono", yearsActive: { min: 2006 } },
+    { value: "Rikiya Koyama", yearsActive: { min: 1987 } },
+]
