1af6ddbb0f2c48dc458d79f79fd8be80710757fc
jtarinelli <tarin22j@mtholyoke.edu>
2025-01-20 23:02:27 -0500

    anime_info query doesn't fire twice on guess

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react"
 
-interface ButtonProps  {
+interface ButtonProps {
     label: String,
-    onClick: (event: any) => void,
+    onClick?: (event: any) => void,
+    type?: "button" | "submit" | "reset",
 }
 
-const Button: FC<ButtonProps> = ({ label, onClick }) => {
-    return <button onClick={onClick} className="bg-accent-400 hover:bg-accent-300 p-2 px-4">{label}</button>
+const Button: FC<ButtonProps> = ({ label, type, onClick }) => {
+    return <button onClick={onClick} type={type} className="bg-accent-400 hover:bg-accent-300 p-2 px-4">{label}</button>
 }
 
 export default Button
